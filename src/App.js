04f905555808f3c62be43d7f720a0bcb3702da14
Rob Gruen <rob@robs-mbp.lan>
2022-10-14 14:45:38 -0400

    Make page title editable by user

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState(''); // [value, setValue]
+  const [title, setTitle] = useState('Monsters Rolodex');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
@@ -29,10 +30,21 @@ const App = () => {
     setSearchField(searchFieldString);
   }
 
+  const onSetTitleChange = (event) => {
+    const titleString = event.target.value;
+
+    if (titleString.length > 0) {
+      setTitle(titleString);
+    } else {
+      setTitle('Monsters Rolodex');
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Monsters Rolodex</h1>
+      <h1>{ title }</h1>
       <SearchBox onSearchChangeHandler={ onSearchChange } placeholderText="Search by name" />
+      <SearchBox onSearchChangeHandler={ onSetTitleChange } placeholderText="Set page title" />
       <CardList monsters={ filteredMonsters } />
     </div>
   );
